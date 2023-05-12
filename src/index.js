e1f43e1b5e8efb0903faccd77284e87e7f2c44bd
Itishree602 <122922402+Itishree602@users.noreply.github.com>
2023-05-12 13:18:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,41 @@
-const app = require('./app');
-const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const seedWithDummyData = require('../seeder');
 
+/*
+Add custom validation to the User model
+Validate that the username is at least 4 characters long, doesn't contain spaces, and doesn't contain special characters
+Also add a validation for checking password length. Password should be atleast 8 chaaacters long.
+Throw an error is the Validation fails.
+*/
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: function(v) {
+                    return /^[^\s\W]{4,}$/.test(v);
+                },
+                message: props => `${props.value} is not a valid username! Username should be at least 4 characters long, without spaces or special characters!`
+            }
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true,
+            validate: {
+                validator: function(v) {
+                    return /^.{8,}$/.test(v);
+                },
+                message: props => `${props.value} is not a valid password! Password should be at least 8 characters long!`
+            }
+        }
+    },
+    { timestamps: true }
+);
 
-dotenv.config();
-//connect to DB
-mongoose.set('useCreateIndex', true);
-mongoose.set('useFindAndModify', false);
-mongoose.connect(process.env.DATABASE_URL ,{ useNewUrlParser: true, useUnifiedTopology: true }, async () => {
-    console.log('connected to DB');
-    await seedWithDummyData();
-})
-
-app.listen(3000, () => console.log('Server running......'));
+module.exports = mongoose.model('User', userSchema);
